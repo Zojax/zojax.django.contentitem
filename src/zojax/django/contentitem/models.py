95fdaa8d787bde386406f8e51034e62d1c3937bb
bubenkoff <bubenkoff@65ff189f-35ee-0310-953c-23049635c3cf>
2011-01-18 16:43:14 +0000

    site support added

--- src/zojax/django/contentitem/models.py
+++ src/zojax/django/contentitem/models.py
@@ -1,16 +1,61 @@
 from autoslug.fields import AutoSlugField
 from django.db import models
+from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 import datetime
+from django.conf import settings
+from django.db import models
+from django.db.models.fields import FieldDoesNotExist
 
 
-class ContentItemManager(models.Manager):
+class CurrentSiteManager(models.Manager):
+    "Use this to limit objects to those associated with the current site."
+    
+    __field_name = 'sites'
+    __is_validated = False
+    
+    def __init__(self, field_name='sites'):
+        super(CurrentSiteManager, self).__init__()
+        self.__field_name = field_name
+        self.__is_validated = False
+
+    def get_query_set(self):
+        if not self.__is_validated:
+            try:
+                self.model._meta.get_field(self.__field_name)
+            except FieldDoesNotExist:
+                raise ValueError("%s couldn't find a field named %s in %s." % \
+                    (self.__class__.__name__, self.__field_name, self.model._meta.object_name))
+            self.__is_validated = True
+        return super(CurrentSiteManager, self).get_query_set().filter(models.Q(**{self.__field_name + '__in': [settings.SITE_ID]}) | models.Q(**{self.__field_name + '__isnull': True}))
+
+
+class CurrentSiteModelMixin(models.Model):
+    
+    sites = models.ManyToManyField(Site, blank=True, related_name="%(app_label)s_%(class)s_related")
+    
+    class Meta:
+        abstract = True
+    
+    def save(self, *args, **kwargs):
+        super(CurrentSiteModelMixin, self).save(*args, **kwargs)
+
+        requires_save = False
+        if not len(self.sites.all()):
+            self.sites = [Site.objects.get_current()]
+            requires_save = True
+
+        if requires_save:
+            super(CurrentSiteModelMixin, self).save(*args, **kwargs)
+
+
+class ContentItemManager(CurrentSiteManager):
     
     def published(self):
         return super(ContentItemManager, self).get_query_set().filter(published=True) 
 
 
-class ContentItem(models.Model):
+class ContentItem(CurrentSiteModelMixin):
     
     title = models.CharField(max_length=300)
     slug = AutoSlugField(populate_from='title', verbose_name=_(u"Slug"), always_update=True)
